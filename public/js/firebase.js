af8a2815208863d0345a2f09af0ad4e08271db0d
Atsuki-522 <atsukitennis0522@gmail.com>
2024-11-03 15:14:31 -0800

    First commit

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+     const firebaseConfig = {
+        apiKey: "AIzaSyARjA4q8spLbcwLxOoYQoB7u2d5SCC25Hc",
+        authDomain: "ist105midtermexam-atsuk.firebaseapp.com",
+        projectId: "ist105midtermexam-atsuk",
+        storageBucket: "ist105midtermexam-atsuk.appspot.com",
+        messagingSenderId: "25553160760",
+        appId: "1:25553160760:web:4f8d9c6f6fef31f087b4cd"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
